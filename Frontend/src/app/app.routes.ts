803b19f5858c63c4f7b6f85ec1d4650cac3976e3
DOTANLOC <itcsiu21199@student.hcmiu.edu.vn>
2025-10-09 15:34:50 +0700

    update routes using authguard and guestguard

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { authGuard } from './services/auth.guard';
+import { guestGuard } from './services/guest.guard';
 
 export const routes: Routes = [
 
     {path: 'home', title: 'Home', component: HomeComponent},
-    {path: 'login', title: 'Login', component: LoginComponent},
-    {path: 'register', title: 'Register', component: RegisterComponent},
-    {path: 'profile', title: 'Profile', component: ProfileComponent},
+    {path: 'login', title: 'Login', component: LoginComponent, canActivate: [guestGuard]},
+    {path: 'register', title: 'Register', component: RegisterComponent, canActivate: [guestGuard]},
+    {path: 'profile', title: 'Profile', component: ProfileComponent, canActivate: [authGuard], data: {roles: ['user']}},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', redirectTo: '/home'},
 ];
