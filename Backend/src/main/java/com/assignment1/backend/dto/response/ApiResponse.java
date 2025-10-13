6612d01e830a89a6e16f3fee1371d9c9481d23a6
DOTANLOC <itcsiu21199@student.hcmiu.edu.vn>
2025-10-13 15:17:07 +0700

    dto/response

--- Backend/src/main/java/com/assignment1/backend/dto/response/ApiResponse.java
+++ Backend/src/main/java/com/assignment1/backend/dto/response/ApiResponse.java
@@ -1,5 +1,26 @@
 package com.assignment1.backend.dto.response;
 
-public class ApiResponse {
-    
+import lombok.Data;
+
+@Data
+public class ApiResponse<T> {
+    private String message;
+    private boolean success;
+    private T data;
+    private String error;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = true;
+        response.message = message;
+        response.data = data;
+        return response;
+    }
+
+    public static <T>ApiResponse<T> fail(String error) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = false;
+        response.error = error;
+        return response;
+    }
 }
--- Backend/src/main/java/com/assignment1/backend/dto/response/AuthResponse.java
+++ Backend/src/main/java/com/assignment1/backend/dto/response/AuthResponse.java
@@ -1,5 +1,22 @@
 package com.assignment1.backend.dto.response;
 
+import lombok.Data;
+
+@Data
 public class AuthResponse {
-    
+    private String accessToken;
+    private String refreshToken;
+    private Integer expiresIn;
+    private Integer refreshExpiresIn;
+    private String tokenType;
+    private UserInfo user;
+
+    @Data
+    public static class UserInfo {
+        private String sub;
+        private String usernma;
+        private String email;
+        private String firstName;
+        private String lastName;
+    }
 }
