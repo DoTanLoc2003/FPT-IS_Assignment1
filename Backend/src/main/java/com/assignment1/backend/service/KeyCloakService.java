29ec752843f86c56c89819ec5e3a3599bdb6bdc4
DOTANLOC <itcsiu21199@student.hcmiu.edu.vn>
2025-10-13 15:28:16 +0700

    init services

---
+++ Backend/src/main/java/com/assignment1/backend/service/KeyCloakService.java
@@ -0,0 +1,49 @@
+package com.assignment1.backend.service;
+
+import com.assignment1.backend.dto.request.LoginRequest;
+import com.assignment1.backend.dto.request.RegisterRequest;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KeyCloakService {
+    
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Value("${keycloak.auth-server-url}")
+    private String keycloakAuthServerUrl;
+
+    @Value("${keycloak.realm}")
+    private String keycloakRealm;
+
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    @Value("${keycloak.admin-username}")
+    private String adminUsername;
+
+    @Value("${keycloak.admin-password}")
+    private String adminPassword;
+
+}
--- Backend/src/main/java/com/assignment1/backend/service/UserService.java
+++ Backend/src/main/java/com/assignment1/backend/service/UserService.java
@@ -1,1 +1,27 @@
 package com.assignment1.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.assignment1.backend.dto.request.RegisterRequest;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    // @Autowired
+    // private KeyCloakService keyCloakService;
+
+    public void register(RegisterRequest registerRequest) {
+
+        try {
+            RegisterRequest request = new RegisterRequest();
+            request.setUsername(registerRequest.getUsername());
+            request.setEmail(registerRequest.getEmail());
+            request.setPassword(registerRequest.getPassword());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create user " + e.getMessage());
+        }
+    }
+        
+}
