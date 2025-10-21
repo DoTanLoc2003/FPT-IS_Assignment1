38f3575288014900d19276c3ae58e3d65dd29a96
DOTANLOC <itcsiu21199@student.hcmiu.edu.vn>
2025-10-21 15:36:25 +0700

    add DataSourceAutoConfiguration to test without database

--- Backend/src/main/java/com/assignment1/backend/BackendApplication.java
+++ Backend/src/main/java/com/assignment1/backend/BackendApplication.java
@@ -2,14 +2,17 @@ package com.assignment1.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		System.out.print("Start at localhost:8081");
 	}
 
 	@Bean
